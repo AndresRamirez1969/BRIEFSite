8d9616ad07e1b1ccffce661cfce53244d7185ac0
AndresRamirez1969 <andres.ramirezdea@gmail.com>
2025-05-07 16:11:30 -0600

    Landing page updates

--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import atrocity from '../../imgs/atrocity.jpg'
+import tomorrow from '../../imgs/tomorrow.png'
 import './landingpage.css'
+import { SpotifyOutlined, YoutubeOutlined } from '@ant-design/icons';
+
 
 const LandingPage = () => {
     return (
+        <div className="landing-wrapper">
         <div style={{ display: 'flex', alignItems: 'flex-start'}}>
             <img 
                 src={atrocity} 
@@ -11,21 +15,47 @@ const LandingPage = () => {
                 style={{ width: '600px', height: 'auto', margin: 0, padding: 0, display: 'block'}} />
         <div style={{ textAlign: 'center', padding: '2rem', flex: 1}}>
             <h1 className='custom-font'>Escucha Nuestro Nuevo Sencillo</h1>
-            <iframe 
-                style={{ borderRadius: '12px'}} 
-                src="https://open.spotify.com/embed/track/3qVLf1ZkJwT1rq60rvMeb1?utm_source=generator" 
-                width="100%" 
-                height="152" 
-                frameBorder="0" 
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                loading="lazy"
-                title="Spotify Player"
+            <iframe
+                width="100%"
+                height="400"
+                src="https://www.youtube.com/embed/AcoIgzjy-uQ"
+                title="Youtube player"
+                allow="accelerometer; autoplay; local-fonts; fullscreen;"
+                style={{ border: "none"}}
             ></iframe>
-            <a href="https://linktr.ee/brief_band" target='_blank' rel="noopener noreferrer" className='button-link'
-            >
-                Mas Canciones
+            <div className='streaming-links'>
+            <a href="https://open.spotify.com/track/3qVLf1ZkJwT1rq60rvMeb1?si=5f6f8f4fbe9447f7" target='_blank' rel="noopener noreferrer" className='spotify-link'>
+                <SpotifyOutlined style={{ fontSize: '35px'}}/>
             </a>
+            <a href="https://music.youtube.com/watch?v=S-2Po3uVQno" target='_blank' rel="noopener noreferrer" className='youtube-link'>
+                <YoutubeOutlined style={{ fontSize: '35px'}}/>
+            </a>
+                
+            </div>
         </div>
+        </div>
+            <div className="tomorrow-image">
+                <img src={tomorrow} alt="Tomorrow image" />
+                <div style={{ textAlign: 'center', padding: '2rem', flex: 1}}>
+                    <h1 className='custom-font'>Escucha Tomorrow</h1>
+                    <iframe
+                        width="100%"
+                        height="400"
+                        src="https://www.youtube.com/embed/AbO-v0WwVmo"
+                        title="Youtube player"
+                        allow="accelerometer; autoplay; local-fonts; fullscreen;"
+                        style={{ border: "none"}}
+                    ></iframe>
+                    <div className='streaming-links'>
+                        <a href="https://open.spotify.com/track/3qVLf1ZkJwT1rq60rvMeb1?si=5f6f8f4fbe9447f7" target='_blank' rel="noopener noreferrer" className='spotify-link'>
+                            <SpotifyOutlined style={{ fontSize: '35px'}}/>
+                        </a>
+                        <a href="https://music.youtube.com/watch?v=S-2Po3uVQno" target='_blank' rel="noopener noreferrer" className='youtube-link'>
+                            <YoutubeOutlined style={{ fontSize: '35px'}}/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
